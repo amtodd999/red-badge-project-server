18df2fe0c4a8cc4c96ee6d831a99d4aba0035e08
Angie Todd <a.m.todd999@gmail.com>
2021-09-30 20:15:50 -0400

    trying to get past heroku 503 error

--- db.js
+++ db.js
@@ -1,11 +1,26 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/<dbName>`, {
     dialect: `postgres`,
-    ssl: process.env.ENVIRONMENT === 'production'
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 })
 // const sequelize = new Sequelize(process.env.DB_URL, {
 //     dialect: `postgres`,
 //ssl: process.env.ENVIRONMENT === `production`
+// })
+
+// const sequelize = new Sequelize(process.env.DB_URL, {
+//     dialect: ‘postgres’,
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
 // })
 
 module.exports = sequelize;
